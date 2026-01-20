f59f2036c5ede963c32d5cdb3645dceefae402d2
abdo <abdelrahmanabuharag@gmail.com>
2026-01-20 19:44:42 +0200

    removing non-used vars

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import './Loader.css';
 
 const Loader = ({ onFinish }) => {
     const [progress, setProgress] = useState(0);
-    const [showButton, setShowButton] = useState(false);
 
     useEffect(() => {
         // Simulate loading progress
