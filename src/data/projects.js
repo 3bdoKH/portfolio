16859078de90f92ed241f0c1db0cb32191b2a610
Abdulrahman Khairy Fawzy <146169707+3bdoKH@users.noreply.github.com>
2026-01-30 22:15:48 +0200

    Rename projects array to dummyProjects

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ import project4 from '../images/project-4.png';
 import project5 from '../images/project-5.png';
 import project6 from '../images/project-6.png';
 
-export const projects = [
+export const dummyProjects = [
     {
         id: 1,
         title: 'project_one',
